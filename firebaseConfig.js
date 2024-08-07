006219cdc2e66baec056b798123b59796f194a6b
Shad <plucks.tailed_0w@icloud.com>
2024-08-07 23:52:14 +0100

    Remove the dotenv package

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -1,13 +1,17 @@
-if (process.env.NODE_ENV !== "production") {
-  require("dotenv").config();
-}
-
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
 import { firebaseConfig } from "./firebaseConfigObj";
 
 // Your web app's Firebase configuration
+// export const firebaseConfig = {
+//   apiKey: process.env.FIREBASE_API_KEY,
+//   authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+//   projectId: process.env.FIREBASE_PROJECT_ID,
+//   storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+//   messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+//   appId: process.env.FIREBASE_APP_ID,
+// };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
