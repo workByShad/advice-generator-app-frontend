d9447efe047ba5c29ce41f0a50799d74623c562a
Shad <plucks.tailed_0w@icloud.com>
2024-08-08 02:16:34 +0100

    Clean up comments

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -1,17 +1,9 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
-import { firebaseConfig } from "./firebaseConfigObj";
 
 // Your web app's Firebase configuration
-// export const firebaseConfig = {
-//   apiKey: process.env.FIREBASE_API_KEY,
-//   authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-//   projectId: process.env.FIREBASE_PROJECT_ID,
-//   storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-//   messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-//   appId: process.env.FIREBASE_APP_ID,
-// };
+import { firebaseConfig } from "./firebaseConfigObj";
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
