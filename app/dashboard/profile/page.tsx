52d7b3542c4c7b5fa7863b14c9850926630ac84a
Shad <plucks.tailed_0w@icloud.com>
2024-09-05 22:15:06 +0100

    remove useless code

--- app/dashboard/profile/page.tsx
+++ app/dashboard/profile/page.tsx
@@ -9,15 +9,13 @@ export default function Profile(): React.ReactElement {
     return (
       <main>
         <Card>
-          <div className="flex flex-col justify-evenly items-start w-full h-full border-4 border-red-500">
-            <h1 className="text-gray-200 text-3xl">Your Profile:</h1>
-            <div className="flex flex-col justify-evenly items-start w-full h-[50%] border-4 border-red-500">
-              <p className="text-gray-200 text-lg">
-                Username: {curUser.displayName}
-              </p>
-              <p className="text-gray-200 text-lg">Email: {curUser.email}</p>
-              <p className="text-gray-200 text-lg">User ID: {curUser.uid}</p>
-            </div>
+          <h1 className="text-gray-200 text-3xl">Your Profile:</h1>
+          <div className="flex flex-col justify-evenly items-start w-full h-[50%]">
+            <p className="text-gray-200 text-lg">
+              Username: {curUser.displayName}
+            </p>
+            <p className="text-gray-200 text-lg">Email: {curUser.email}</p>
+            <p className="text-gray-200 text-lg">User ID: {curUser.uid}</p>
           </div>
         </Card>
       </main>
