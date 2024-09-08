b323c7ad577f9aeb71be8dc93c4b83cdfa72698e
Shad <plucks.tailed_0w@icloud.com>
2024-09-08 18:38:10 +0100

    add config details

---
+++ firebaseConfigObj.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyB4Gnv20CUokQWTKkCDEGZ5-Td4i2ashqk",
+  authDomain: "advice-generator-app-6050a.firebaseapp.com",
+  projectId: "advice-generator-app-6050a",
+  storageBucket: "advice-generator-app-6050a.appspot.com",
+  messagingSenderId: "179432024709",
+  appId: "1:179432024709:web:7fa221ed9bafa4c4f057ec",
+};
